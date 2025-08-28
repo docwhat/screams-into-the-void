990e0003ea1b941e647e3bb41d13619b385e56bb
Christian Höltje <docwhat@gerf.org>
2025-08-28 19:47:00 -0400

    checkpoint

--- Global.gd
+++ Global.gd
@@ -1,4 +1,11 @@
 extends Node
 
-var rng : RandomNumberGenerator
+var resolution := Vector2(
+    ProjectSettings.get_setting("display/window/size/viewport_width"),
+    ProjectSettings.get_setting("display/window/size/viewport_height")
+)
+
+var rng : RandomNumberGenerator = RandomNumberGenerator.new()
 var star_speed : float = 0.01
+
+const player_size : Vector2 = Vector2(64, 64)
