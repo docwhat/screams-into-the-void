057c2e7c3923aa1a3990d40232d9db075c81e87c
Christian Höltje <docwhat@gerf.org>
2025-08-30 01:28:09 -0400

    add asteroid system variables

--- Global.gd
+++ Global.gd
@@ -9,3 +9,7 @@ var rng : RandomNumberGenerator = RandomNumberGenerator.new()
 var star_speed : float = 0.01
 
 const player_size : Vector2 = Vector2(64, 64)
+
+# Asteroid system variables
+var asteroid_player_intercept_chance : float = 0.3  # 30% chance asteroids target player
+var asteroid_top_down_bias : float = 0.7  # 70% chance asteroids come from top
