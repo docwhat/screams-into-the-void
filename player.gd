b54aa9c9a145f978251dd441fbbe40343153e1f7
Christian Höltje <docwhat@gerf.org>
2025-08-30 03:41:11 -0400

    checkpoint

--- player.gd
+++ player.gd
@@ -3,12 +3,12 @@ class_name Player extends Area2D
 func recenter_player():
   var screen_size = get_viewport_rect().size
 
-  $AnimatedSprite2D.position.x = 0
-  $AnimatedSprite2D.position.y = (screen_size.y / 2.0) - Global.player_size.y / 1.5
-  
+  $AnimatedSprite2D.position.x = screen_size.x / 2.0
+  $AnimatedSprite2D.position.y = screen_size.y - Global.player_size.y / 1.5
+
 func _ready() -> void:
   # Values must be initialized before running anything else.
   recenter_player()
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
   recenter_player()
