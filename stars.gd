1a9664e49748a1f9b5417457592a587242536aee
Christian Höltje <docwhat@gerf.org>
2025-10-04 03:28:42 -0400

    sync time in stars shader
    Now the packground moves smoothly when unpaused and stops when paused.

--- stars.gd
+++ stars.gd
@@ -1,9 +1,21 @@
 extends ColorRect
 
 var last_star_speed: float = 0.0
+var time: float = 0.0
 
 
-func _process(_delta: float) -> void:
+func _ready() -> void:
+	time = Global.rng.randf_range(1.0, 40_000.0)
+	_update_shader()
+
+
+func _process(delta: float) -> void:
+	time += delta
+	_update_shader()
+
+
+func _update_shader() -> void:
+	self.material.set_shader_parameter("time", time)
 	if last_star_speed != Global.star_speed:
 		self.material.set_shader_parameter("speed", Global.star_speed)
 		last_star_speed = Global.star_speed
