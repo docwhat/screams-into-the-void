64f73d43374c783b7045276fc4c8ed2390d46c25
Christian Höltje <docwhat@gerf.org>
2025-10-13 00:44:53 -0400

    add option to select number formatting to state

--- state.gd
+++ state.gd
@@ -4,15 +4,22 @@ var matter: MatterBag = MatterBag.new()
 
 var absorber: Absorber = StarterAbsorber.new()
 
-@export_group("Preferences")
+@export_group("Options")
 
+## Use the short symbols or the full names for matter?
 @export var use_symbols: bool = false
+
+## Which number format should we use?
 @export var use_format: NumberTools.NumberFormat = NumberTools.NumberFormat.NONE
-@export var number_grouping_separator: NumberTools.NumberGroupSeparator = (
-	NumberTools.NumberGroupSeparator.COMMA
-)
+
+## What should we use to separate decimals from fractions?
 @export var number_decimal_separator: NumberTools.NumberDecimalSeparator = (
 	NumberTools.NumberDecimalSeparator.PERIOD
+)
+
+## What should we use to separate number groups. i.e., the comma thousands in the USA.
+@export var number_grouping_separator: NumberTools.NumberGroupSeparator = (
+	NumberTools.NumberGroupSeparator.COMMA
 )
 
 @export_group("")
