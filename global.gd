d78c02ed7df1e3698781077504ed91dd40eae732
Christian Höltje <docwhat@gerf.org>
2025-11-17 20:06:16 -0500

    chore: bump version to v0.5.11

--- global.gd
+++ global.gd
@@ -1,6 +1,6 @@
 extends Node
 
-const VERSION: String = "v0.5.10"
+const VERSION: String = "v0.5.11"
 
 var resolution: Vector2 = Vector2(
 	ProjectSettings.get_setting("display/window/size/viewport_width"),
