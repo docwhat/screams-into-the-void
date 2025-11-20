7ec10367073904f385b29dee33496b881cf04f73
Christian Höltje <docwhat@gerf.org>
2025-11-19 21:43:07 -0500

    fix: hide crosshairs when paused

--- main/play_field_state.gd
+++ main/play_field_state.gd
@@ -3,11 +3,13 @@ extends State
 func enter(_previous_state_path: String, _data: Dictionary = { }) -> void:
 	get_tree().set_pause(false)
 	Input.mouse_mode = Input.MOUSE_MODE_CONFINED_HIDDEN
+	%Pointer.show()
 
 
 func exit() -> void:
 	get_tree().set_pause(true)
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	%Pointer.hide()
 
 
 func pause_or_back() -> void:
